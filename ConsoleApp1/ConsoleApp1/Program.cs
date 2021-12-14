c00b2d8c7059375d4b05db230dced05e6727ecfb
Mahammad07 <94453965+Mahammad07@users.noreply.github.com>
2021-12-14 01:14:14 -0600

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using MaqaLibrary;
+using System;
+
+namespace Calculation
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Count = new Calculator();//toplama
+            Count.Countt(3,7);
+
+            Calculator Count2 = new Calculator();//cixma
+            Count2.Countt2(4, 5);
+
+            Calculator Count3 = new Calculator();//bolme
+            Console.WriteLine(Count3.Countt3(10,5));
+
+            Calculator Count4 = new Calculator();//Vurma
+            Console.WriteLine(Count4.Countt4(3,8));
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/MaqaLibrary/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MaqaLibrary
+{
+    public class Calculator
+    {
+        
+        
+
+        public void Countt(int num1,int num2)
+        {
+            Console.WriteLine(num1+num2);
+        }
+
+        public void Countt2(int num1,int num2)
+        {
+            Console.WriteLine(num1-num2);
+        }
+
+        public int Countt3(int num1,int num2)
+        {
+            return num1 / num2;
+        }
+
+       public int Countt4(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+
+
+    }
+}
